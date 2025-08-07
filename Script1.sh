ee2d8ae48befecb879f0a41beb474bd500e86bf5
sanket Sangale <sanket.sangale49@gmail.com>
2025-08-07 12:33:50 +0530

    This is my first commit

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "This is a test script for our DevOps training"
-echo "We are learning how to create a script in Linux"
+echo "My name is Khan"
